98507bedea1a28bcb416aa14fc6ed788d78d0bbd
garrettmichael <garesnap@gmail.com>
2023-06-07 15:43:40 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+html body {
+  background-color: #161a20;
+  color: #ba2e55;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div>
+    Hello! Site is currently under construction. In the meantime, please
+    checkout my GitHub to see what I'm currently working on!
+  </div>
+  <h1>
+    <a href="http://github.com/garretthensley" target="_blank">My GitHub</a>
+  </h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
